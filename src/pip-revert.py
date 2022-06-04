9035f1298d9d51aca137f06d89563de51073d07f
yoni2 <yoni24536@gmail.com>
2022-06-04 17:29:32 +0300

    revert func

--- src/pip-revert.py
+++ src/pip-revert.py
@@ -5,20 +5,17 @@ import time
 
 from pathlib import Path
 from configparser import ConfigParser
+
 import sys
 
-SNAPSHOT_PREFIX = 'pip-snapshot'
 SNAPSHOTS_DIR = 'Snapshots'
-
-def main(*args):
-    print(args)
 
 
 def snapshot():
-    args = parse_args()
-    snapshot_file_name = SNAPSHOT_PREFIX + str(int(time.time()))
+    args = parse_snapshot_args()
+    snapshot_file_name = str(int(time.time())) if args.tag is None else args.tag
     if args.path is None:
-        pip_snapshot_location = _find_pip_location() / SNAPSHOTS_DIR
+        pip_snapshot_location = _snapshot_dir_location()
         if not os.path.isdir(pip_snapshot_location):
             os.mkdir(pip_snapshot_location)
         snapshot_path = pip_snapshot_location / snapshot_file_name
@@ -27,24 +24,45 @@ def snapshot():
             raise NotADirectoryError()
         snapshot_path = Path(args.path) / snapshot_file_name
 
-    snapshot_file = open(snapshot_path, 'w+')
-    subprocess.call(['pip', 'freeze', '>', snapshot_path], stdout=snapshot_file)
+    snapshot_file = open(snapshot_path, 'w')
+    subprocess.call(['pip', 'freeze'], stdout=snapshot_file)
 
     print('Successfully saved snapshot to: {}'.format(snapshot_path))
+    return snapshot_path
 
 
-def parse_args():
+def revert():
+
+    last_snapshot_path = max(list(_snapshot_dir_location().glob('*')), key=os.path.getmtime)
+    current_snapshot = snapshot()
+
+    try:
+        subprocess.run(['pip', 'install', '-r', last_snapshot_path])
+        os.remove(last_snapshot_path)
+    except ...:
+        subprocess.run(['pip', 'install', '-r', current_snapshot])
+
+
+def parse_snapshot_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', default=None, type=str, help='Path to venv root')
+    parser.add_argument('--path', default=None, type=str)
+    parser.add_argument('--tag', default=None, type=str)
     return parser.parse_args()
 
 
-def _find_pip_location() -> Path:
+def parse_revert_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', default=None, type=str)
+    parser.add_argument('--tag', default=None, type=str)
+    return parser.parse_args()
+
+
+def _snapshot_dir_location() -> Path:
     pip_config = subprocess.check_output(['pip', 'show', 'pip'], shell=True).decode()
     config_parser = ConfigParser()
     config_parser.read_string('[header_stub]' + pip_config)
 
-    return Path(config_parser.get('header_stub', 'Location'))
+    return Path(config_parser.get('header_stub', 'Location')) / SNAPSHOTS_DIR
 
 
-snapshot()
+revert()
