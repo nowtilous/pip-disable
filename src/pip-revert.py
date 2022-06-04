f313915a5932259bcedaf8d51736ff4a09164699
yoni2 <yoni24536@gmail.com>
2022-06-04 18:33:20 +0300

    polish revert func

--- src/pip-revert.py
+++ src/pip-revert.py
@@ -28,19 +28,29 @@ def snapshot():
     subprocess.call(['pip', 'freeze'], stdout=snapshot_file)
 
     print('Successfully saved snapshot to: {}'.format(snapshot_path))
-    return snapshot_path
 
 
 def revert():
+    args = parse_revert_args()
+    snapshots = sorted(_snapshot_dir_location().iterdir(), key=os.path.getmtime)
+    if len(snapshots) == 0:
+        raise NoSnapshotsAvailable()
 
-    last_snapshot_path = max(list(_snapshot_dir_location().glob('*')), key=os.path.getmtime)
-    current_snapshot = snapshot()
+    if args.tag is not None:
+        if not os.path.isfile(args.tag):
+            raise FileNotFoundError()
+        last_snapshot_path = _snapshot_dir_location() / args.tag
+    elif args.n is not None:
+        if args.n < 1 or args.n > len(snapshots):
+            raise ValueError("Invalid revert count specified! Available range is 1 - {}".format(len(snapshots)))
+        snapshots.reverse()
+        last_snapshot_path = snapshots[args.n - 1]
+    else:
+        last_snapshot_path = max(snapshots)
 
-    try:
-        subprocess.run(['pip', 'install', '-r', last_snapshot_path])
-        os.remove(last_snapshot_path)
-    except ...:
-        subprocess.run(['pip', 'install', '-r', current_snapshot])
+    subprocess.run(['pip', 'install', '-r', last_snapshot_path])
+    os.remove(last_snapshot_path)
+    print("Successfully reverted to snapshot '{}'".format(last_snapshot_path))
 
 
 def parse_snapshot_args():
@@ -52,7 +62,7 @@ def parse_snapshot_args():
 
 def parse_revert_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', default=None, type=str)
+    parser.add_argument('-n', default=None, type=int)
     parser.add_argument('--tag', default=None, type=str)
     return parser.parse_args()
 
@@ -63,6 +73,10 @@ def _snapshot_dir_location() -> Path:
     config_parser.read_string('[header_stub]' + pip_config)
 
     return Path(config_parser.get('header_stub', 'Location')) / SNAPSHOTS_DIR
+
+
+class NoSnapshotsAvailable(Exception):
+    pass
 
 
 revert()
